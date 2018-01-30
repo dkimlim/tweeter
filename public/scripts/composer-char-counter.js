719ed084370f1c8bcd76ee337209170f4fe3d93d
Diane KL <dianekimlim@gmail.com>
2018-01-30 21:46:17 +0000

    refactored function to update counter based on number of characters in the tweet post

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,12 +1,20 @@
 // Function responsible for the character count in the text-form to post on Tweeter
-$(document).ready(function(){ 
-  var counter = 140 - Number(length);
-  var length = 0;
-$( "textarea" ).keyup(function() {
-	length++;
-  }
-	console.log(length);
-});
-// console.log(counter);
 
+$(document).ready(function(){ 
+  console.log('start');
+
+  const maxCounter = 140;
+
+	$('textarea').on('keyup', (function() {
+	    let characters = $(this).val().length;
+	    let counter = $(this).parent().children('.counter')
+
+	    counter.text(140 - characters);
+	  
+		if (characters > maxCounter) {
+		   counter.css('color', 'red');
+		} else {
+		   counter.css('color', 'black');
+		}
+	}));
 });
