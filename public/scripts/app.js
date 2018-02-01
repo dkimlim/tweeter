c461290e6e0e56f283f89b477e3b69480a654872
Diane KL <dianekimlim@gmail.com>
2018-02-01 02:05:52 +0000

    added some code

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -7,8 +7,8 @@
  $(document).ready(function(){ 
  	console.log('start');
 
- 	//Render the single tweet to append it to the main container. 
 	
+ 	//Render the single tweet to append it to the main container. 
 	function renderTweets(tweets) {
 		const articles_elm = $('.tweets-container');
 
@@ -17,7 +17,7 @@
 		})
 	};	
 
-	//Get a single tweet from a database.
+	//Get a single tweet from the database.
 	function createTweetElement (article) {
 	  let $tweet = 
 	  	`<article class="tweet">
@@ -30,9 +30,77 @@
 	  return $tweet;
 	};
 
-	// renderTweets(data);
+// beforeSend: function() {                    
+//     $empty = $('form#yourForm').find("input").filter(function() {
+//         return this.value === "";
+//     });
+//     if($empty.length) {
+//         alert('You must fill out all fields in order to submit a change');
+// 	// // POST data of new tweet with Ajax and rendering it back to main index page;
+// 	function validateTweet(form) {
+//     if(this.text.value == "") {
+//       alert("Don't leave me empty!");
+//       this.text.focus();
+//       return false;
+//     }
+//     if(this.text.value == "" || !this.valid_email.checked) {
+//       alert("Please enter a valid Email address");
+//       this.email.focus();
+//       return false;
+//     }
+//     if(this.age.value == "" || !this.valid_age.checked) {
+//       alert("Please enter an Age between 16 and 100");
+//       this.age.focus();
+//       return false;
+//     }
+//     alert("Success!  The form has been completed, validated and is ready to be submitted...");
+//     return false;
+//   }
+
+// 	$("form").submit(validate() {
+
+//     if(this.text.value == ""})
+
+//     submitHandler: function(form) {
+//         $.ajax({
+//             url: form.action,
+//             type: form.method,
+//             data: $(form).serialize(),
+//             success: function(response) {
+//                 $('#answers').html(response);
+//             }            
+//         });
+//     }
+// });
+
+// 	$("form").validate({
+//   invalidHandler: function(event, validator) {
+//     // 'this' refers to the form
+//     var errors = validator.numberOfInvalids();
+//     if (errors) {
+//       var message = errors == 1
+//         ? 'You missed 1 field. It has been highlighted'
+//         : 'You missed ' + errors + ' fields. They have been highlighted';
+//       $("div.error span").html(message);
+//       $("div.error").show();
+//     } else {
+//       $("div.error").hide();
+//     }
+//   }
+// });
+
 
 	$("form").submit(function( event ) {
+	    event.preventDefault();
+
+	    let formText = $("#textTweet");
+
+	    console.log(formText.text());
+
+
+
+
+
 		$.ajax({
 			url: '/tweets/',
 			dataType: 'text',
@@ -45,35 +113,36 @@
 			},
 			error: handleError('loadArticles')
 		})
-	  
-	  event.preventDefault();
 	});
+		// }}
+
+		$( document ).ajaxError(function( event, request, settings ) {
+  $( "#msg" ).append( "<li>Error requesting page " + settings.url + "</li>" );
+});
+	  
 
 	const handleError = (label) => {
 	    return (err) => {
-	      // we can access to label here even though this function
-	      // is being invoked later
-	      console.debug(`Error @ ${label}`, err);
+	    console.debug(`Error @ ${label}`, err);
 	    }
   	}
 
-	// $.get('/tweets/')
-	// .then(renderTweets)
-	// .fail(handleError('loadArticles'));
+	
+	// const textArea = $('.textTweet');
 
-	// $(".tweets-container"function loadTweets(){
-	// 	$.ajax({
-	// 		url: '/tweets/',
-	// 		method: 'GET'
-	// 		// success: function renderTweets(data){
+	// if (textArea.length > 140) {
+	// 	   alert('tweet is too long');
 
-	// 		// },
-	// 		// error: function handleError(loadedArticles){
+	// 	   console.log(textArea)
+	// 	   console.log(textArea.length)
 
-	// 		// }
-	// 	});
-	// });
-	// console.log(loadTweets);
+	// 	   console.log('tweet is too long')
+
+	// 	} else if (textArea === "") {
+	// 	   alert('tweet is empty');
+		
+	// 	} else {
+
 
 });
 
