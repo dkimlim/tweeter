5741855c37e16400af55a1d3d9ff8c019cffe79b
Diane KL <dianekimlim@gmail.com>
2018-02-04 20:18:04 +0000

    fixed counter and added library to calculate the moment a tweet is created in minutes and days

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -14,14 +14,25 @@ $(document).ready(function(){
 	function createTweetElement (article) {
 	  let $tweet = 
 	  	`<article class="tweet">
-	    <header><img src ="${article["user"].avatars.small}">
-	    <span class="name">${article["user"].name}</span>
-		<span class="username">${article["user"].handle}</span></header>
-	    <p>${escape(article["content"].text)}</p>
-	    <footer class="time-ago">${article.created_at}</footer>
+
+	        <header>
+	            <img src ="${article["user"].avatars.small}">
+	            <span class="name">${article["user"].name}</span>
+		        <span class="username">${article["user"].handle}</span>
+		    </header>
+	        
+	        <p>${escape(article["content"].text)}</p>
+	    
+	        <footer class="time-ago">${moment(article.created_at).fromNow()}</footer>
+
+
 	     </article>`;
 	  return $tweet;
 	};
+
+	            // const $footer = $('<footer>').text(moment(tweetObj.created_at).fromNow());
+
+// Math.round(your_elapsed_time/60000)
 
 	//Escape function to prevent XSS in text area of tweet-form.
 	function escape(str) {
@@ -30,24 +41,57 @@ $(document).ready(function(){
  	  return div.innerHTML;
 	}
 
-	//GET method with Ajax 
-	function getTweet() {
-			$.get('/tweets/')
-			.then(renderTweets)
-			.fail(handleError('loadArticles'));
-	}	
+	//Button "compose" in the header toggles the new-tweet form and focuses on the text area.
+  	$('.new-tweet').hide();
+  	$('button').click(function(event) {
+  		$('.new-tweet').toggle();
+  		$('#textTweet').focus().select();
+  	});
 
-	//function to handle errors when Ajax is called
+	// //all this code is for appending specific age of tweet with appropriate text beside it
+ //        //converting time in milliseconds to days
+ //        let timeSinceTweet = ((Date.now() - tweetObject.created_at) / (1000 * 60 * 60 * 24));
+ //        let hours = false;
+ //        //if days < 1, multiplying by 24 to get amount of hours ago
+ //        if(timeSinceTweet < 1) {
+ //            timeSinceTweet *= 24;
+ //            hours = true;
+ //        }
+    
+ //        if(hours) {
+ //            //if hours is less than 1
+ //            if(timeSinceTweet < 1) {
+ //                let mins = Math.floor(timeSinceTweet * 60);
+ //                $footer.append(`<p class='posted_date'>${mins} minutes ago`);
+ //            } else {
+ //                $footer.append(`<p class='posted_date'>${Math.floor(timeSinceTweet)} hours ago`);
+ //            }
+ //        //if not in hours (still days) then print [x] days old 
+ //        } else {
+ //            $footer.append(`<p class='posted_date'>${Math.floor(timeSinceTweet)} days ago`);
+ //        }
+ //        return $tweet;
+ //    }    
+    
 	const handleError = (label) => {
 	    return (err) => {
 	    	console.debug(`Error @ ${label}`, err);
 	    }
   	}
+	//GET data from mongoDB with Ajax to load tweets on default page.
+	function loadTweets() {
+			$.get('/tweets/')
+			.then(renderTweets)
+			.fail(handleError('loadArticles'));
+	}	
+	loadTweets();
+
+	//function to handle errors when Ajax is called
 	
 	//Click button on form has 2 conditionals before passing to AJAX Post request:
 	// - cannot submit empty text (return error message)
 	// - cannot submit a textarea more than 140 characters (return error message)
-	const $button = $('#tweetButton');
+	const $button = $('.tweetButton');
 
 	$("form").submit(function(event) {
 	    event.preventDefault();
@@ -75,13 +119,10 @@ $(document).ready(function(){
 				.fail(handleError('loadArticles'));
 			}	
 		})
+		//clear text area and counter of 140 after posting tweet
+        $("#textTweet").val("");
+        $(".counter").text(140);
 	});
-
-  	//Button "compose" in the header toggles the new-tweet form
-  	$('.new-tweet').hide();
-  	$("button").click(function(event) {
-  		$('.new-tweet').toggle();
-  	});
 //end DOM
 }); 
 
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,6 +1,19 @@
 
 // Function responsible for the character count in the text-form POST on Tweeter
 
+
+// $(document).ready(function() {
+//     $( "#textTweet" ).keyup(function(ev) {
+//         let charAmount = $(this).val().length;
+//         let newCount = 140 - charAmount;
+//         $(this).parent().children( ".counter" ).html(newCount);
+//         if (newCount < 0) {
+//             $(this).parent().children( ".counter" ).css("color", "Red");
+//         } else {
+//             $(this).parent().children( ".counter" ).css("color", "Black")
+//         }
+//       });
+//   });
 $(document).ready(function(){ 
 
   const maxCounter = 140;
